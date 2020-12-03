1447d4d850762887b45b2dbce19d07ef4eb97060
Yann Moisan <yamo93@gmail.com>
2020-12-03 07:15:34 +0100

    day 3

---
+++ 2020/src/main/scala/Day3.scala
@@ -0,0 +1,18 @@
+object Day3 extends MultiPuzzle[Long, Long] {
+  override def part1(input: Iterator[String]): Long = {
+    val grid = input.toArray
+    countTrees(grid, 3, 1)
+  }
+
+  @SuppressWarnings(Array("org.wartremover.warts.TraversableOps"))
+  override def part2(input: Iterator[String]): Long = {
+    val grid = input.toArray
+    List((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))
+      .map { case (right, down) => countTrees(grid, right, down) }
+      .reduce(_ * _)
+  }
+
+  def countTrees(grid: Array[String], right: Int, down: Int): Long =
+    (0 until grid.length / down)
+      .count(i => grid(i * down)(i * right % grid.head.length) == '#').toLong
+}
--- 2020/src/main/scala/Puzzles.scala
+++ 2020/src/main/scala/Puzzles.scala
@@ -1,1 +1,1 @@
-object PuzzlesApp extends Puzzles(List(Day1))
+object PuzzlesApp extends Puzzles(List(Day1, Day2, Day3))

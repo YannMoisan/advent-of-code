8cf60cf58bd2d2d179c9465227140133d3a440f2
Yann Moisan <yann@zen.ly>
2022-11-20 22:26:49 +0100

    [2015] Day 24 clean code

--- 2015/src/main/scala/Day24.scala
+++ 2015/src/main/scala/Day24.scala
@@ -1,24 +1,13 @@
+@SuppressWarnings(Array("org.wartremover.warts.OptionPartial"))
 object Day24 extends MultiPuzzle[Long, Long] {
-  override def part1(input: Iterator[String]): Long = {
-    val total = input.toList.map(_.toLong)
-    println(total.sum/4)
-    println(total.combinations(2).filter(_.sum == 508).size)
-    println(total.combinations(3).filter(_.sum == 508).size)
-    println(total.combinations(4).filter(_.sum == 508).size)
-    println(total.combinations(5).filter(_.sum == 508).size)
-    println(total.combinations(6).filter(_.sum == 508).size)
-    println(total.combinations(7).filter(_.sum == 508).size)
-    total.combinations(6).filter(_.sum == 508).minBy(_.product).product
-  }
+  override def part1(input: Iterator[String]): Long = common(input, 3)
 
-  override def part2(input: Iterator[String]): Long = {
+  override def part2(input: Iterator[String]): Long = common(input, 4)
+
+  private def common(input: Iterator[String], groupCount: Int): Long = {
     val total = input.toList.map(_.toLong)
-    println(total.combinations(2).filter(_.sum == 381).size)
-    println(total.combinations(3).filter(_.sum == 381).size)
-    println(total.combinations(4).filter(_.sum == 381).size)
-    println(total.combinations(5).filter(_.sum == 381).size)
-    println(total.combinations(6).filter(_.sum == 381).size)
-    println(total.combinations(7).filter(_.sum == 381).size)
-    total.combinations(5).filter(_.sum == 381).minBy(_.product).product
+    val target = total.sum / groupCount
+    val count = (1 to total.length).find(i => (total.combinations(i).exists(_.sum == target)))
+    total.combinations(count.get).filter(_.sum == target).minBy(_.product).product
   }
 }

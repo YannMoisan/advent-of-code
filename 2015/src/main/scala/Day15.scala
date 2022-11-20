1679a7e90f0961ebd317f6596e7c371f7c3f6da4
Yann Moisan <yann@zen.ly>
2022-11-20 21:15:08 +0100

    [2015] Day 15 perf

--- 2015/src/main/scala/Day15.scala
+++ 2015/src/main/scala/Day15.scala
@@ -4,10 +4,9 @@ object Day15 extends MultiPuzzle[Int, Int] {
   override def part1(input: Iterator[String]): Int = {
     val tmp = for {
       a <- 0 to 100
-      b <- 0 to 100
-      c <- 0 to 100
-      d <- 0 to 100
-      if a + b + c + d == 100
+      b <- 0 to 100 - a
+      c <- 0 to 100 - (a + b)
+      d = 100 - (a + b + c)
     } yield {
       val aa = math.max(0, 2 * a)
       val bb = math.max(0, 5 * b - d)
@@ -21,10 +20,9 @@ object Day15 extends MultiPuzzle[Int, Int] {
   override def part2(input: Iterator[String]): Int = {
     val tmp = for {
       a <- 0 to 100
-      b <- 0 to 100
-      c <- 0 to 100
-      d <- 0 to 100
-      if a + b + c + d == 100
+      b <- 0 to 100 - a
+      c <- 0 to 100 - (a + b)
+      d = 100 - (a + b + c)
       if 3 * a + 3 * b + 8 * c + 8 * d == 500
     } yield {
       val aa = math.max(0, 2 * a)

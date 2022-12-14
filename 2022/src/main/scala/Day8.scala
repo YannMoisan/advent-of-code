cfdd673e879dfbc1e5fb45161021fc26c5b92486
Yann Moisan <yann@zen.ly>
2022-12-14 11:13:48 +0100

    Y2022D8 use grid

--- 2022/src/main/scala/Day8.scala
+++ 2022/src/main/scala/Day8.scala
@@ -1,35 +1,35 @@
 @SuppressWarnings(Array("org.wartremover.warts.TraversableOps"))
 object Day8 extends MultiPuzzle[Int, Int]{
   override def part1(input: Iterator[String]): Int = {
-    val grid: Array[Array[Int]] = input.toArray.map(s => s.map(c => c.toString.toInt).toArray)
+    val grid = new Grid(input.toArray.map(_.toArray))
     (for {
-      row <- grid.indices
-      col <- grid.head.indices
-      if isVisible(grid, row, col)
-    } yield (row, col)).size
+      x <- grid.xindices
+      y <- grid.yindices
+      if isVisible(grid, Pos(x,y))
+    } yield Pos(x,y)).size
   }
 
   override def part2(input: Iterator[String]): Int = {
-    val grid: Array[Array[Int]] = input.toArray.map(s => s.map(c => c.toString.toInt).toArray)
+    val grid = new Grid(input.toArray.map(_.toArray))
     (for {
-      row <- 1 until grid.length - 1
-      col <- 1 until grid.head.length - 1
-    } yield score(grid, row, col)).max
+      x <- 1 until grid.width - 1
+      y <- 1 until grid.height - 1
+    } yield score(grid, Pos(x,y))).max
   }
 
-  private def isVisible(grid: Array[Array[Int]], r: Int, c: Int) =
-    views(grid, r, c).exists(xs => xs.forall(x => grid(x._1)(x._2) < grid(r)(c)))
+  private def isVisible(grid: Grid, p: Pos) =
+    views(grid, p).exists(xs => xs.forall(x => grid(x) < grid(p)))
 
-  private def views(grid: Array[Array[Int]], r: Int, c: Int) = Seq(
-    (0 until r).map((_, c)).reverse,
-    (r + 1 until grid.head.length).map((_, c)),
-    (0 until c).map((r, _)).reverse,
-    (c + 1 until grid.length).map((r, _))
+  private def views(grid: Grid, p: Pos) = Seq(
+    (0 until p.y).map(Pos(p.x, _)).reverse,
+    (p.y + 1 until grid.height).map(Pos(p.x, _)),
+    (0 until p.x).map(Pos(_, p.y)).reverse,
+    (p.x + 1 until grid.width).map(Pos(_, p.y))
   )
 
-  private def score(grid: Array[Array[Int]], r: Int, c: Int) = {
-    views(grid, r, c)
-      .map { xs => math.min(xs.length, xs.takeWhile { pos => grid(pos._1)(pos._2) < grid(r)(c) }.length + 1) }
+  private def score(grid: Grid, p: Pos) = {
+    views(grid, p)
+      .map { xs => math.min(xs.length, xs.takeWhile { pos => grid(pos) < grid(p) }.length + 1) }
       .product
   }
 }

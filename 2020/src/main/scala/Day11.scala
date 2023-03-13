082f505b88e6585338de3ad4a81443b02c98ec55
Yann Moisan <yann@zen.ly>
2023-03-13 11:14:33 +0100

    still grid

--- 2020/src/main/scala/Day11.scala
+++ 2020/src/main/scala/Day11.scala
@@ -1,17 +1,17 @@
-import com.yannmoisan.util.grid.{Direction8, Grid2D, Pos}
+import com.yannmoisan.util.grid.{Direction8, Grid1D, Pos}
 
 object Day11 extends MultiPuzzle[Int, Int] {
   override def part1(input: Iterator[String]): Int = {
-    val grid = new Grid2D(input.toArray.map(_.toArray))
+    val grid = Grid1D(input.toArray)
     run(grid, pos => grid.dim.neighbors8(pos), 4)
   }
 
   override def part2(input: Iterator[String]): Int = {
-    val grid = new Grid2D(input.toArray.map(_.toArray))
+    val grid = Grid1D(input.toArray)
     run(grid, pos => Direction8.all.flatMap(dir => nextVisible(grid, pos, dir)).toArray, 5)
   }
 
-  private def run(grid: Grid2D[Char], neighbors: Pos => Array[Pos], limit: Int): Int =
+  private def run(grid: Grid1D[Char], neighbors: Pos => Array[Pos], limit: Int): Int =
     Iterator
       .iterate(grid)(applyRules(neighbors, limit))
       .sliding(2).find(seq => seq(0) == seq(1)) match {
@@ -19,20 +19,20 @@ object Day11 extends MultiPuzzle[Int, Int] {
       case None      => sys.error("illegal state")
     }
 
-  private def applyRules(neighbors: Pos => Array[Pos], limit: Int)(grid: Grid2D[Char]): Grid2D[Char] = {
-    val newGrid = Grid2D.fill(grid.dim.width, grid.dim.height)('?')
+  private def applyRules(neighbors: Pos => Array[Pos], limit: Int)(grid: Grid1D[Char]): Grid1D[Char] = {
+    val newGrid = Grid1D.fill(grid.dim.width, grid.dim.height)('?')
     newGrid.dim.allPos.foreach { pos =>
-      grid(pos) match {
-        case '.'                                                       => newGrid(pos) = '.'
-        case 'L' if neighbors(pos).count(p => grid(p) == '#') == 0     => newGrid(pos) = '#'
-        case '#' if neighbors(pos).count(p => grid(p) == '#') >= limit => newGrid(pos) = 'L'
-        case _                                                         => newGrid(pos) = grid(pos)
+      newGrid(pos) = grid(pos) match {
+        case '.'                                                       => '.'
+        case 'L' if neighbors(pos).count(p => grid(p) == '#') == 0     => '#'
+        case '#' if neighbors(pos).count(p => grid(p) == '#') >= limit => 'L'
+        case _                                                         => grid(pos)
       }
     }
     newGrid
   }
 
-  private def nextVisible(grid: Grid2D[Char], start: Pos, dir: (Int, Int)): Option[Pos] =
+  private def nextVisible(grid: Grid1D[Char], start: Pos, dir: (Int, Int)): Option[Pos] =
     Iterator
       .unfold(start)(p => grid.dim.move(p, dir).map(x => (x, x)))
       .find(p => grid(p) != '.')
--- core/src/main/scala/com/yannmoisan/util/grid/Grid1D.scala
+++ core/src/main/scala/com/yannmoisan/util/grid/Grid1D.scala
@@ -26,20 +26,25 @@ class Grid1D[@specialized(Int, Char, Boolean) A](private val grid: Array[A], wid
         var ret = true
         while (ret && i < dim.allPos.length) {
           val p = dim.allPos(i)
-          if (this.apply(p) == other(p)) ret = false
+          if (this.apply(p) != other(p)) ret = false
           i += 1
         }
-        true
+        ret
 
       case _ => false
     }
-
 
   override val dim: Dimension = Dimension(width, height)
 }
 
 object Grid1D {
+  def fill[@specialized(Int, Char, Boolean) A: ClassTag](width: Int, height: Int)(elem: => A) : Grid1D[A] = new Grid1D(Array.fill(width * height)(elem), width, height)
+
   def apply[@specialized(Int, Char, Boolean) A:ClassTag](grid: Array[Array[A]]): Grid1D[A] = {
       new Grid1D(grid.flatten, grid.head.length, grid.length)
     }
+
+  def apply(grid: Array[String]): Grid1D[Char] = {
+    new Grid1D(grid.flatten, grid.head.length, grid.length)
+  }
 }

f969acb16a73b52359b097b1da9ffe5ae7ad3c4a
Yann Moisan <yann@zen.ly>
2023-03-16 13:04:34 +0100

    Y2021D15 use Grid

--- 2021/src/main/scala/Day15.scala
+++ 2021/src/main/scala/Day15.scala
@@ -1,9 +1,11 @@
+import com.yannmoisan.util.grid.{Grid, Grid1D, Pos}
+
 @SuppressWarnings(Array("org.wartremover.warts.TraversableOps"))
 object Day15 extends MultiPuzzle[Int, Int] {
   override def part1(input: Iterator[String]) =
-    dijkstra(input.toArray.map(_.toCharArray.map(_.asDigit)))
+    dijkstra(Grid1D(input.toArray.map(_.toCharArray.map(_.asDigit))))
 
-  override def part2(input: Iterator[String]) = dijkstra(fiveTimesLarger(input.toArray.map(_.toCharArray.map(_.asDigit))))
+  override def part2(input: Iterator[String]) = dijkstra(Grid1D(fiveTimesLarger(input.toArray.map(_.toCharArray.map(_.asDigit)))))
 
   def fiveTimesLarger(input: Array[Array[Int]]): Array[Array[Int]] =
     Array
@@ -14,37 +16,19 @@ object Day15 extends MultiPuzzle[Int, Int] {
         if (value > 9) value - 9 else value
       }
 
-  def apply(lines: Array[Array[Int]], x: Int, y: Int): Int =
-    lines(y)(x) //.asDigit
-
-  def neighbors(mx: Int, my: Int, x: Int, y: Int): Seq[(Int, Int)] =
-    Seq(
-      (x + 1, y),
-      (x - 1, y),
-      (x, y + 1),
-      (x, y - 1)
-    ).filter { case (x, y) => x >= 0 && y >= 0 && x < mx && y < my }
-
-  def dijkstra(grid: Array[Array[Int]]) = {
-    val mx: Int = grid.head.length
-    val my: Int = grid.length
-    val dist    = Array.fill[Int](mx, my)(Int.MaxValue)
-    dist(0)(0) = 0
-    var toVisit: Set[(Int, Int)] = (for {
-      x <- 0 until mx
-      y <- 0 until my
-    } yield (x, y)).toSet
+  def dijkstra(grid: Grid[Int]) = {
+    val dist    = Array.fill[Int](grid.dim.width * grid.dim.height)(Int.MaxValue)
+    dist(0) = 0
+    var toVisit = grid.dim.indices.toSet
     while (toVisit.size > 0) {
       // find min
-      val s1 = toVisit.minBy { case (x, y) => dist(y)(x) }
+      val s1 = toVisit.minBy { index => dist(index) }
       toVisit -= s1
-      neighbors(mx, my, s1._1, s1._2)
-        .foreach {
-          case (x, y) =>
-            if (dist(y)(x) > dist(s1._2)(s1._1) + apply(grid, x, y))
-              dist(y)(x) = dist(s1._2)(s1._1) + apply(grid, x, y)
+      grid.dim.neighbors4(s1).foreach { j =>
+            if (dist(j) > dist(s1) + grid(j))
+              dist(j) = dist(s1) + grid(j)
         }
     }
-    dist(my - 1)(mx - 1)
+    dist(Pos(grid.dim.width - 1, grid.dim.height - 1)(grid.dim).index)
   }
 }

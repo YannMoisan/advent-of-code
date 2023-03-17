367591d2ea7a8d3d2ad695c39ba2b75ddaee4103
Yann Moisan <yann@zen.ly>
2023-03-17 13:41:31 +0100

    Y2015D4 remove while

--- 2015/src/main/scala/Day4.scala
+++ 2015/src/main/scala/Day4.scala
@@ -2,30 +2,22 @@ import java.security.MessageDigest
 
 object Day4 extends SinglePuzzle[Int, Int] {
   override def part1(input: String): Int = {
-    val md    = MessageDigest.getInstance("MD5")
-    var i     = 0
-    var found = false
-    while (!found) {
-      val s = md
-        .digest(s"$input$i".getBytes)
-        .map(b => f"$b%02x")
-        .mkString
-      if (s.startsWith("00000")) found = true else i += 1
-    }
-    i
+    val md = MessageDigest.getInstance("MD5")
+    Iterator
+      .from(0).map(i =>
+        md.digest(s"$input$i".getBytes)
+          .map(b => f"$b%02x")
+          .mkString
+      ).indexWhere(_.startsWith("00000"))
   }
 
   override def part2(input: String): Int = {
-    val md    = MessageDigest.getInstance("MD5")
-    var i     = 0
-    var found = false
-    while (!found) {
-      val s = md
-        .digest(s"$input$i".getBytes)
-        .map(b => f"$b%02x")
-        .mkString
-      if (s.startsWith("000000")) found = true else i += 1
-    }
-    i
+    val md = MessageDigest.getInstance("MD5")
+    Iterator
+      .from(0).map(i =>
+        md.digest(s"$input$i".getBytes)
+          .map(b => f"$b%02x")
+          .mkString
+      ).indexWhere(_.startsWith("000000"))
   }
 }

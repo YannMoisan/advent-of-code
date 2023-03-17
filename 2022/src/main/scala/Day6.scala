d540077115a16d77851ce9113e95eb9a99b2b343
Yann Moisan <yann@zen.ly>
2023-03-17 18:18:42 +0100

    Y2022D6 remove while

--- 2022/src/main/scala/Day6.scala
+++ 2022/src/main/scala/Day6.scala
@@ -1,17 +1,14 @@
+@SuppressWarnings(Array("org.wartremover.warts.OptionPartial"))
 object Day6 extends SinglePuzzle[Int, Int] {
-  override def part1(input: String): Int = {
-    var i = 3
-    while (i < input.length && (i - 3 to i).map(input(_)).toSet.size != 4) {
-      i += 1
-    }
-    i + 1
-  }
+  override def part1(input: String): Int =
+    Iterator
+      .from(3)
+      .find(i => (i - 3 to i).map(input(_)).toSet.size == 4)
+      .get + 1
 
-  override def part2(input: String): Int = {
-    var i = 13
-    while (i < input.length && (i - 13 to i).map(input(_)).toSet.size != 14) {
-      i += 1
-    }
-    i + 1
-  }
+  override def part2(input: String): Int =
+    Iterator
+      .from(13)
+      .find(i => (i - 13 to i).map(input(_)).toSet.size == 14)
+      .get + 1
 }

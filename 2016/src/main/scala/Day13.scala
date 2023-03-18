aae0b7ff333c9dc9b6680252616a2755902a02e1
Yann Moisan <yann@zen.ly>
2023-03-18 21:10:11 +0100

    Y2016D13 use grid

--- 2016/src/main/scala/Day13.scala
+++ 2016/src/main/scala/Day13.scala
@@ -1,45 +1,29 @@
+import com.yannmoisan.util.grid.{Grid, Grid1D, Pos}
+
 object Day13 extends SinglePuzzle[Int, Int] {
 
-  type Grid = Seq[String]
+  def moves(grid: Grid[Char]): Int => Seq[Int] = { i =>
+    grid.dim.neighbors4(i).filter(grid(_) == '.')
+  }
 
-  case class Pos(x: Int, y: Int)
-
-  def possibleMoves(p: Pos): Seq[Pos] = Seq(
-    Pos(p.x - 1, p.y),
-    Pos(p.x + 1, p.y),
-    Pos(p.x, p.y - 1),
-    Pos(p.x, p.y + 1)
-  )
-
-  def validPos(g: Grid)(p: Pos) =
-    p.x >= 0 &&
-      p.x < g(0).length &&
-      p.y >= 0 &&
-      p.y < g.size &&
-      g(p.y)(p.x) == '.'
-
-  val grid = generateMaze(100, 100, 1350)
-
-  def moves: Pos => Seq[Pos] = p => possibleMoves(p).filter(validPos(grid))
-
-  def generateMaze(w: Int, h: Int, fav: Int): Grid =
-    (0 until h).map(y => (0 until w).map(x => isNumber(x, y, fav)).mkString(""))
-
-  def print(g: Grid) = g.foreach(println)
+  def generateMaze(w: Int, h: Int, fav: Int): Grid[Char] =
+    Grid1D(Array.tabulate(100, 100) { case (y, x) => isNumber(x, y, fav) })
 
   def isNumber(x: Long, y: Long, fav: Long): Char =
     if ((x * x + 3 * x + 2 * x * y + y + y * y + fav).toBinaryString.count(_ == '1') % 2 == 0) '.'
     else '#'
 
   override def part1(s: String): Int = {
-    val stream = BFS.breadth_first_traverse(Pos(1, 1), moves)
-    stream.find(_._1 == Pos(31, 39)).get._2.length - 1
+    val grid: Grid[Char] = generateMaze(100, 100, 1350)
+    val stream           = BFS.breadth_first_traverse(Pos(1, 1)(grid.dim).index, moves(grid))
+    stream.find(_._1 == Pos(31, 39)(grid.dim).index).get._2.length - 1
   }
 
   override def part2(s: String): Int = {
-    val stream            = BFS.breadth_first_traverse(Pos(1, 1), moves)
+    val grid: Grid[Char]  = generateMaze(100, 100, 1350)
+    val stream            = BFS.breadth_first_traverse(Pos(1, 1)(grid.dim).index, moves(grid))
     val limitedStream     = stream.takeWhile(_._2.length <= 51)
-    val visited: Set[Pos] = limitedStream.map(_._1).toSet
+    val visited: Set[Int] = limitedStream.map(_._1).toSet
     visited.size
   }
 

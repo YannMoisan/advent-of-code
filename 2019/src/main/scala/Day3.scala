b453548855f2a7fdeddad8260726ec286442bbf9
Yann Moisan <yann@zen.ly>
2023-03-19 13:09:26 +0100

    Y2019D3 use position

--- 2019/src/main/scala/Day3.scala
+++ 2019/src/main/scala/Day3.scala
@@ -1,6 +1,5 @@
+import com.yannmoisan.util.geo.Position
 import com.yannmoisan.util.grid.Direction
-
-import scala.collection.Iterator
 
 object Day3 extends MultiPuzzle[Int, Int] {
   override def part1(lines: Iterator[String]): Int = {
@@ -21,29 +20,27 @@ object Day3 extends MultiPuzzle[Int, Int] {
     intersection.map(pos => path1.indexOf(pos) + path2.indexOf(pos) + 2).min
   }
 
-  def parseLine(l: String): Array[Move] = l.split(",").map(s => Move(s.head, s.tail.toInt))
-
-  def fullPath(path: Array[Move]): Vector[Pos] = {
-    val init = State(Pos(0, 0), Vector.empty[Pos])
-
-    val state = path.foldLeft(init) {
-      case (state, move) =>
-        val range = (1 to move.length)
-        val dir = move.direction match {
-          case 'D' => Direction.Down
-          case 'U' => Direction.Up
-          case 'R' => Direction.Right
-          case 'L' => Direction.Left
-        }
-        val newPath =
-          range.map(i => Pos(state.pos.x + i * dir.delta._1, state.pos.y + i * dir.delta._2))
-
-        State(newPath.last, state.fullPath ++ newPath)
+  def parseLine(l: String): Array[Move] = l.split(",").map { s =>
+    val dir = s.head match {
+      case 'D' => Direction.Down
+      case 'U' => Direction.Up
+      case 'R' => Direction.Right
+      case 'L' => Direction.Left
     }
-    state.fullPath
+    Move(dir, s.tail.toInt)
+  }
+
+  def fullPath(path: Array[Move]): Vector[Position] = {
+    val init = Vector(Position(0, 0))
+
+    val positions = path.foldLeft(init) {
+      case (state, move) =>
+        val newPath =
+          Iterator.iterate(state.last)(Position.move(_, move.direction)).drop(1).take(move.length)
+        state ++ newPath
+    }
+    positions.drop(1) // remove Position(0,0)
   }
 }
 
-final case class Move(direction: Char, length: Int)
-final case class Pos(x: Int, y: Int)
-final case class State(pos: Pos, fullPath: Vector[Pos])
+final case class Move(direction: Direction, length: Int)

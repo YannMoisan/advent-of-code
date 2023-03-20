721dee67cec5ed7321f4d107fae6515547c06a35
Yann Moisan <yann@zen.ly>
2023-03-20 09:20:21 +0100

    Y2016D1 remove state monad

--- 2016/src/main/scala/Day1.scala
+++ 2016/src/main/scala/Day1.scala
@@ -1,6 +1,3 @@
-import cats.data.State
-import cats.instances.all._
-import cats.syntax.traverse._
 import com.yannmoisan.util.collection.{next, prev}
 import com.yannmoisan.util.geo.Position
 import com.yannmoisan.util.grid.Direction
@@ -18,7 +15,7 @@ object Day1 extends SinglePuzzle[Int, Int] {
     def toInstructions(s: String): Seq[Instruction] = s match {
       case "L" => Seq(TurnLeft)
       case "R" => Seq(TurnRight)
-      case s   => List.fill(s.toString.toInt)(Walk)
+      case s   => List.fill(s.toInt)(Walk)
     }
     lines
       .split(", ")
@@ -26,34 +23,29 @@ object Day1 extends SinglePuzzle[Int, Int] {
       .flatMap(toInstructions)
   }
 
-  def update(i: Instruction): PositionAndDir => PositionAndDir =
-    pad =>
-      i match {
-        case TurnLeft  => pad.copy(dir = prev(pad.dir, Direction.all4))
-        case TurnRight => pad.copy(dir = next(pad.dir, Direction.all4))
-        case Walk      => pad.copy(pos = move(pad))
-      }
-
-  def update2(i: Instruction): State[PositionAndDir, Option[Position]] =
-    State.modify(update(i)).inspect(pad => if (i == Walk) Some(pad.pos) else None)
-
-  def move(pad: PositionAndDir): Position =
-    Position.move(pad.pos, pad.dir)
+  def update(pad: PositionAndDir, i: Instruction): PositionAndDir =
+    i match {
+      case TurnLeft  => pad.copy(dir = prev(pad.dir, Direction.all4))
+      case TurnRight => pad.copy(dir = next(pad.dir, Direction.all4))
+      case Walk      => pad.copy(pos = Position.move(pad.pos, pad.dir))
+    }
 
   override def part1(lines: String): Int = {
     val instructions = parse(lines)
-    val state: State[PositionAndDir, List[Unit]] =
-      instructions.map(i => State.modify(update(i))).toList.sequence
-    val finalState = state.runS(PositionAndDir(Position(0, 0), Direction.Up)).value
-    math.abs(finalState.pos.x) + math.abs(finalState.pos.y)
+    val start        = PositionAndDir(Position(0, 0), Direction.Up)
+    val finalState   = instructions.foldLeft(start)(update)
+    Position.manhattan(finalState.pos, Position(0, 0))
   }
 
   override def part2(lines: String): Int = {
-    val instructions = parse(lines).toStream
-    val state        = instructions.map(update2).sequence
-    val positions    = state.runA(PositionAndDir(Position(0, 0), Direction.Up)).value.flatten
-    val p            = firstDuplicate(positions).get
-    math.abs(p.x) + math.abs(p.y)
+    val instructions = parse(lines)
+    val start        = PositionAndDir(Position(0, 0), Direction.Up)
+    val positions    = instructions.scanLeft(start)(update).map(_.pos).toVector
+    val cleanedPositions = positions.zipWithIndex.collect {
+      case (p, i) if i == positions.size - 1 || positions(i) != positions(i + 1) => p
+    }
+    val p = firstDuplicate(cleanedPositions.toStream).get
+    Position.manhattan(p, Position(0, 0))
   }
 
   def firstDuplicate[T](s: Stream[T], seen: Set[T] = Set.empty[T]): Option[T] = s match {

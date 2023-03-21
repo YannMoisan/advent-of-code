ddd3d686f13eea2e9594bb9ee8c2bbafe1f0731f
Yann Moisan <yann@zen.ly>
2023-03-21 12:36:32 +0100

    Y2020D3 use direction

--- 2020/src/main/scala/Day3.scala
+++ 2020/src/main/scala/Day3.scala
@@ -1,17 +1,23 @@
+import com.yannmoisan.util.geo.Position
+import com.yannmoisan.util.grid.{Direction, Grid, Grid1D, Pos}
+
 object Day3 extends MultiPuzzle[Long, Long] {
   override def part1(input: Iterator[String]): Long = {
-    val grid = input.toArray
-    countTrees(grid, 3, 1)
+    val grid = Grid1D(input)
+    countTrees(grid, new Direction(3, 1, -1) {})
   }
 
   override def part2(input: Iterator[String]): Long = {
-    val grid = input.toArray
+    val grid = Grid1D(input)
     List((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))
-      .map { case (right, down) => countTrees(grid, right, down) }
+      .map { case (right, down) => countTrees(grid, new Direction(right, down, -1) {}) }
       .reduce(_ * _)
   }
 
-  def countTrees(grid: Array[String], right: Int, down: Int): Long =
-    (0 until grid.length / down)
-      .count(i => grid(i * down)(i * right % grid.head.length) == '#').toLong
+  def countTrees(grid: Grid[Char], dir: Direction): Long =
+    Iterator
+      .iterate(Position(0, 0))(Position.move(_, dir))
+      .takeWhile(_.y < grid.dim.height)
+      .count(p => grid(Pos(p.x % grid.dim.width, p.y)(grid.dim).index) == '#')
+      .toLong
 }

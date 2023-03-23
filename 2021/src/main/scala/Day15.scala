75e91c3e751afd9fe3ba41a99c059bc4be34755b
Yann Moisan <yann@zen.ly>
2023-03-23 17:45:51 +0100

    Y2021D15 extract dijkstra

--- 2021/src/main/scala/Day15.scala
+++ 2021/src/main/scala/Day15.scala
@@ -1,6 +1,5 @@
-import com.yannmoisan.util.grid.{Grid, Grid1D}
-
-import scala.collection.mutable
+import com.yannmoisan.util.grid.Dijkstra.dijkstra
+import com.yannmoisan.util.grid.Grid1D
 
 object Day15 extends MultiPuzzle[Int, Int] {
   override def part1(input: Iterator[String]) =
@@ -18,23 +17,4 @@ object Day15 extends MultiPuzzle[Int, Int] {
         if (value > 9) value - 9 else value
       }
 
-  def dijkstra(grid: Grid[Int]) = {
-    val dist = Array.fill[Int](grid.dim.size)(Int.MaxValue)
-    dist(0) = 0
-    val toVisit = mutable.TreeSet[(Int, Int)](dist.zipWithIndex.toIndexedSeq: _*)
-
-    while (toVisit.size > 0) {
-      // find min
-      val (d, s1) = toVisit.head
-      val _       = toVisit.remove((d, s1))
-      grid.dim.neighbors4(s1).foreach { j =>
-        if (dist(j) > dist(s1) + grid(j)) {
-          val _ = toVisit.remove((dist(j), j))
-          dist(j) = dist(s1) + grid(j)
-          val _ = toVisit.addOne((dist(j), j))
-        }
-      }
-    }
-    dist(grid.dim.size - 1)
-  }
 }
---
+++ core/src/main/scala/com/yannmoisan/util/grid/Dijkstra.scala
@@ -0,0 +1,26 @@
+package com.yannmoisan.util.grid
+
+import scala.collection.mutable
+
+object Dijkstra {
+  def dijkstra(grid: Grid[Int]) = {
+    val dist = Array.fill[Int](grid.dim.size)(Int.MaxValue)
+    dist(0) = 0
+    val toVisit = mutable.TreeSet[(Int, Int)](dist.zipWithIndex.toIndexedSeq: _*)
+
+    while (toVisit.size > 0) {
+      // find min
+      val (d, s1) = toVisit.head
+      val _ = toVisit.remove((d, s1))
+      grid.dim.neighbors4(s1).foreach { j =>
+        if (dist(j) > dist(s1) + grid(j)) {
+          val _ = toVisit.remove((dist(j), j))
+          dist(j) = dist(s1) + grid(j)
+          val _ = toVisit.addOne((dist(j), j))
+        }
+      }
+    }
+    dist(grid.dim.size - 1)
+  }
+
+}

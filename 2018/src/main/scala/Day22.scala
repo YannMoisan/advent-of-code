ee2c46f5ed3c9b97c522bc8d937f27a837044267
Yann Moisan <yann@zen.ly>
2023-03-27 12:15:14 +0200

    Y2018D22

---
+++ 2018/src/main/scala/Day22.scala
@@ -0,0 +1,40 @@
+import com.yannmoisan.util.grid.{Grid, Grid1D, Pos}
+
+object Day22 extends MultiPuzzle[Int, Int] {
+
+  override def part1(input: Iterator[String]): Int = {
+    val s"depth: $depth_"              = input.next()
+    val s"target: $targetx_,$targety_" = input.next()
+    val depth                          = depth_.toInt
+    val targetx                        = targetx_.toInt
+    val targety                        = targety_.toInt
+
+    val grid = makeGrid(depth, targetx, targety)
+    riskLevel(grid)
+  }
+
+  private def makeGrid(depth: Int, targetx: Int, targety: Int): Grid[Int] = {
+    val grid = Grid1D.fill(targetx + 1, targety + 1)(0)
+    grid.dim.indices.foreach { i =>
+      val geologicIndex = grid.dim.pos(i) match {
+        case Pos(0, 0, _)                 => 0
+        case Pos(`targetx`, `targety`, _) => 0 // stable identifier
+        case Pos(targetx, 0, _)           => targetx * 16807
+        case Pos(0, targety, _)           => targety * 48271
+        case Pos(x, y, _) =>
+          grid(Pos(x - 1, y)(grid.dim).index) * grid(Pos(x, y - 1)(grid.dim).index)
+      }
+      val erosionLevel = (geologicIndex + depth) % 20183
+      grid(i) = erosionLevel
+    }
+    grid
+  }
+
+  private def riskLevel(grid: Grid[Int]): Int = {
+    var sum = 0
+    grid.dim.indices.foreach(i => sum += grid(i) % 3)
+    sum
+  }
+
+  override def part2(input: Iterator[String]): Int = 42
+}

c7850323aaadfde1d49a586d4253e1147fd075a2
Yann Moisan <yann@zen.ly>
2023-03-29 14:18:04 +0200

    perf avoid intermediate arraybuffer

--- 2015/src/main/scala/Day10.scala
+++ 2015/src/main/scala/Day10.scala
@@ -1,5 +1,3 @@
-import scala.collection.mutable
-
 object Day10 extends SinglePuzzle[Int, Int] {
   // conclusion 1 : ArrayBuffer > ListBuffer
 
@@ -16,44 +14,22 @@ object Day10 extends SinglePuzzle[Int, Int] {
   }
 
   def lookAndSay(s: String): String = {
-//    if (s.length < 100) println(s)
-//    println(s"-${s.length}")
-    val buf = new mutable.ArrayBuffer[(Char, Int)](s.length)
-    (0 until s.length).foreach { i =>
-      if (i == 0) {
-        buf.addOne((s(0), 1))
+    val sb           = new StringBuilder()
+    var lastCh: Char = s(0)
+    var count: Int   = 1
+    s.substring(1).foreach { ch =>
+      if (lastCh == ch) {
+        count += 1
       } else {
-        val Some((lastCh, lastCount)) = buf.lastOption
-        if (lastCh == s(i)) {
-          val _ = buf.remove(buf.length - 1)
-          buf.addOne((lastCh, lastCount + 1))
-        } else
-          buf.addOne((s(i), 1))
+        sb.append((count + 48).toChar)
+        sb.append(lastCh)
+        lastCh = ch
+        count = 1
       }
     }
-    val arr = Array.ofDim[Char](buf.length * 2)
-    buf.indices.foreach { i =>
-      arr(i * 2) = (buf(i)._2 + 48).toChar
-      arr(i * 2 + 1) = buf(i)._1
-    }
-    new String(arr)
-  }
+    sb.append((count + 48).toChar)
+    sb.append(lastCh)
 
-//  def lookAndSay(s: String): String = {
-//    println(s"-${s.length}")
-//    val buf = mutable.ListBuffer[(Char, Int)]()
-//    (0 until s.length).foreach { i =>
-//      if (i == 0) {
-//        buf.addOne((s(0), 1))
-//      } else {
-//        val Some((lastCh, lastCount)) = buf.lastOption
-//        if (lastCh == s(i)) {
-//          val _ = buf.remove(buf.length - 1)
-//          buf.addOne((lastCh, lastCount + 1))
-//        } else
-//          buf.addOne((s(i), 1))
-//      }
-//    }
-//    buf.map { case (ch, count) => s"$count$ch" }.mkString
-//  }
+    sb.toString()
+  }
 }

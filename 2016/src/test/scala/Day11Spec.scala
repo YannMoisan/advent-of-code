75c6d030e59f75d954c31703cfcbada4f25f26f7
Yann Moisan <yann@zen.ly>
2023-03-30 12:25:37 +0200

    Y2016D11 perf

--- 2016/src/test/scala/Day11Spec.scala
+++ 2016/src/test/scala/Day11Spec.scala
@@ -1,28 +1,29 @@
+import Day11.Foo
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class Day11Spec extends AnyFlatSpec with Matchers {
-  it should "work on aM" in {
-    Day11.isValidFloor(Set("aM")) shouldBe true
+  it should "work on PM" in {
+    Day11.isValidFloor(Set("PM").map(Foo.apply)) shouldBe true
   }
 
-  it should "work on aG" in {
-    Day11.isValidFloor(Set("aG")) shouldBe true
+  it should "work on PG" in {
+    Day11.isValidFloor(Set("PG").map(Foo.apply)) shouldBe true
   }
 
-  it should "work on aM aG" in {
-    Day11.isValidFloor(Set("aM", "aG")) shouldBe true
+  it should "work on PM PG" in {
+    Day11.isValidFloor(Set("PM", "PG").map(Foo.apply)) shouldBe true
   }
 
-  it should "work on aM bM" in {
-    Day11.isValidFloor(Set("aM", "bM")) shouldBe true
+  it should "work on PM TM" in {
+    Day11.isValidFloor(Set("PM", "TM").map(Foo.apply)) shouldBe true
   }
 
-  it should "work on aM bG" in {
-    Day11.isValidFloor(Set("aM", "bG")) shouldBe false
+  it should "work on PM TG" in {
+    Day11.isValidFloor(Set("PM", "TG").map(Foo.apply)) shouldBe false
   }
 
-  it should "work on aM bM bG" in {
-    Day11.isValidFloor(Set("aM", "bM", "bG")) shouldBe false
+  it should "work on PM TM TG" in {
+    Day11.isValidFloor(Set("PM", "TM", "TG").map(Foo.apply)) shouldBe false
   }
 }

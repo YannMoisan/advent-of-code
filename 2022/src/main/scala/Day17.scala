4e6ec6f49a403824484d34a27413216f77b06b1e
Yann Moisan <yann@zen.ly>
2023-04-04 18:10:08 +0200

    Y2022D17 part2

--- 2022/src/main/scala/Day17.scala
+++ 2022/src/main/scala/Day17.scala
@@ -1,7 +1,9 @@
 import com.yannmoisan.util.grid.{Grid, Grid1D, Pos}
 
-object Day17 extends SinglePuzzle[Int, Int] {
-  private val MaxHeight = 4000
+import scala.collection.mutable
+
+object Day17 extends SinglePuzzle[Int, Long] {
+  private val MaxHeight = 40000
 
   val pieces: Seq[Array[String]] = Seq(
     """####""",
@@ -19,40 +21,79 @@ object Day17 extends SinglePuzzle[Int, Int] {
       |##""".stripMargin
   ).map(_.split("\n"))
 
+  case class Key(rockMod: Int, directionMod: Int)
+  case class Value(firstEmptyLine: Int, rockId: Int)
+  val cache = mutable.Map[Key, Value]()
+
   override def part1(input: String): Int = {
     val grid = Grid1D.fill(7, MaxHeight)('.')
 
-    val directions: Iterator[Char] = Iterator.continually(input.toCharArray).flatten
+    val directions = input
 
-    val rocks = Iterator.continually(pieces).flatten
     /* A new rock begins falling:
     Jet of gas pushes rock left:
     Rock falls 1 unit:
     +-------+*/
 
     // is falling possible ?
-    Iterator.iterate(MaxHeight - 1)(fall(rocks.next(), grid, _, directions)).drop(2022).next()
+    Iterator
+      .iterate((MaxHeight - 1, 0, 0))(t => fall(t._3, grid, t._1, directions, t._2)).drop(2022).next()
 
     MaxHeight - (0 to grid.dim.height - 1).reverse
       .find(y => (0 until 7).forall(x => grid(Pos(x, y)) == '.')).get - 1
   }
 
-  override def part2(input: String): Int = 42
+  // cycle detected key=Key(0,9976), old=Value(37290,1715), new=Value(24190,10265), #rocks=-8550, #height=13100
+  // scala> 1000000000000L % 8550
+  // val res0: Long = 2800
+  //
+  // scala> 1000000000000L / 8550
+  // val res1: Long = 116959064
+
+  override def part2(input: String): Long = {
+    val grid = Grid1D.fill(7, MaxHeight)('.')
+
+    val directions = input
+
+    /* A new rock begins falling:
+    Jet of gas pushes rock left:
+    Rock falls 1 unit:
+    +-------+*/
+
+    // is falling possible ?
+    Iterator
+      .iterate((MaxHeight - 1, 0, 0))(t => fall(t._3, grid, t._1, directions, t._2)).drop(2800).next()
+
+    ((1000000000000L / 8550) * 13100) + MaxHeight - (0 to grid.dim.height - 1).reverse
+      .find(y => (0 until 7).forall(x => grid(Pos(x, y)) == '.')).get - 1
+  }
 
   private def fall(
-      rock: Array[String],
+      rockId: Int,
       grid: Grid[Char],
       firstEmptyLine: Int,
-      directions: Iterator[Char]
-  ): Int = {
+      directions: String,
+      dirIndex: Int
+  ): (Int, Int, Int) = {
     // Each rock appears so that its left edge is two units away from the left wall
     // and its bottom edge is three units above the highest rock in the room (or the floor, if there isn't one).
     var current = Pos(2, firstEmptyLine - 3)
 
-    var continue = true
+    val k = Key(rockId % 5, dirIndex % directions.length)
+    val v = Value(firstEmptyLine, rockId)
+    if (cache.contains(k)) {
+      println(
+        s"cycle detected key=$k, old=${cache(k)}, new=$v, #rocks=${cache(k).rockId - v.rockId}, #height=${cache(k).firstEmptyLine - firstEmptyLine}"
+      )
+    } else cache.put(k, v)
 
+    val rock = pieces(rockId % 5)
+
+    var continue  = true
+    var dirIndex0 = dirIndex
     while (continue) {
-      val dir     = directions.next()
+      val dir = directions(dirIndex0 % directions.size)
+      dirIndex0 += 1
       val xOffset = if (dir == '>') +1 else -1
       if (canMove(rock, grid, Pos(current.x + xOffset, current.y))) {
         current = Pos(current.x + xOffset, current.y)
@@ -63,7 +104,7 @@ object Day17 extends SinglePuzzle[Int, Int] {
     }
 
     rest(rock, grid, current)
-    math.min(firstEmptyLine, current.y - rock.length)
+    (math.min(firstEmptyLine, current.y - rock.length), dirIndex0, rockId + 1)
   }
 
   private def canMove(rock: Array[String], grid: Grid[Char], target: Pos): Boolean =

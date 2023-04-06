d430ec79eb31172afd1d8f764a3ef9fb8a8c50bc
Yann Moisan <yann@zen.ly>
2023-04-06 14:58:12 +0200

    Y2022D19 2 optims

--- 2022/src/main/scala/Day19.scala
+++ 2022/src/main/scala/Day19.scala
@@ -1,39 +1,67 @@
 import com.yannmoisan.util.graph.BFS
 
 object Day19 extends MultiPuzzle[Int, Int] {
-//  // Blueprint 1:
-//  //  Each ore robot costs 4 ore.
-//  //  Each clay robot costs 2 ore.
-//  //  Each obsidian robot costs 3 ore and 14 clay.
-//  //  Each geode robot costs 2 ore and 7 obsidian.
-//  val blueprint1: Seq[Array[Int]] = Vector(
-//    Array(4, 0, 0, 0),  // ore robot
-//    Array(2, 0, 0, 0),  // clay robot
-//    Array(3, 14, 0, 0), // obsidian robot
-//    Array(2, 0, 7, 0)   // geode robot
-//  )
 
-  case class Blueprint(id: Int, costs: Vector[Array[Int]])
+  case class Blueprint(id: Int, costs: Vector[Array[Int]]) {
+    val max = (0 to 3).map(resourceIndex => (0 to 3).map(costs(_)(resourceIndex)).max)
+  }
 
-  case class State(minute: Int, resources: Vector[Int], robots: Vector[Int])
+  case class State(
+      minute: Int,
+      resources: Vector[Int],
+      robots: Vector[Int],
+      canBuildPrev: Array[Boolean]
+  )
 
-  def next(bp: Blueprint)(s: State): Seq[State] =
-    if (s.minute == 25) Seq.empty
+  def next(bp: Blueprint, maxTime: Int)(s: State): Seq[State] =
+    if (s.minute == maxTime) Seq.empty
     else {
       val updatedResources = Vector.tabulate(4)(i => s.resources(i) + s.robots(i))
-      State(
-        s.minute + 1,
-        updatedResources,
-        s.robots
-      ) +: (0 to 3)
-        .filter(i => s.minute != 24 && (0 to 3).forall(j => bp.costs(i)(j) <= s.resources(j))).map {
-          i =>
+      // What are the type of robots that I can build if I wait enough time
+      // and the count is below the max
+      val canBuild: Array[Boolean] = Array(
+        s.robots(0) < bp.max(0), // we produce ore
+        s.robots(1) < bp.max(1), // we produce ore
+        s.robots(2) < bp.max(2) && s
+          .robots(1) > 0, // we need to produce clay     to build an obsidian robot
+        s.robots(2) > 0   // we need to produce obsidian to build a  geode    robot
+      )
+
+      // Do we have enough resources to build this robot
+      val canBuildNow = (0 to 3).map { robotId =>
+        (0 to 3).forall(j => bp.costs(robotId)(j) <= s.resources(j))
+      }.toArray
+
+      // If we have enough resources to build all robot possible => ONLY build
+      // Else build & wait
+      val build: Seq[State] =
+        (0 to 3)
+          .filter(i =>
+            s.minute != maxTime - 1 && canBuildNow(i) && canBuild(i) && !s.canBuildPrev(i)
+          ).map(i =>
             State(
               s.minute + 1,
               Vector.tabulate(4)(k => updatedResources(k) - bp.costs(i)(k)),
-              s.robots.updated(i, s.robots(i) + 1)
+              s.robots.updated(i, s.robots(i) + 1),
+              Array(false, false, false, false)
             )
-        }
+          )
+
+      val wait = State(
+        s.minute + 1,
+        updatedResources,
+        s.robots,
+        canBuildNow
+      )
+
+      if (s.minute != maxTime - 1 && canBuildNow(3) && canBuild(3) && !s.canBuildPrev(3)) {
+        Seq(build.last)
+      } else {
+        if (canBuild.count(_ == true) == canBuildNow.count(_ == true))
+          build
+        else
+          wait +: build
+      }
     }
 
   override def part1(input: Iterator[String]): Int = {
@@ -49,18 +77,30 @@ object Day19 extends MultiPuzzle[Int, Int] {
           )
         )
     }
-    blueprints.map(qualityLevel).sum
+    blueprints.map(bp => geodes(bp, 24) * bp.id).sum
   }
 
-  def qualityLevel(blueprint: Blueprint): Int = {
-    println(blueprint.id)
-    val init = State(1, Vector(0, 0, 0, 0), Vector(1, 0, 0, 0))
-    val geodes = BFS
-      .breadth_first_traverse_no_path_it(init, next(blueprint)).filter(_.minute == 24).maxBy(
+  def geodes(blueprint: Blueprint, maxTime: Int): Int = {
+    val init = State(0, Vector(0, 0, 0, 0), Vector(1, 0, 0, 0), Array(false, false, false, false))
+    BFS
+      .breadth_first_traverse_no_path_it(init, next(blueprint, maxTime)).filter(_.minute == maxTime).maxBy(
         _.resources(3)
       ).resources(3)
-    geodes * blueprint.id
   }
 
-  override def part2(input: Iterator[String]): Int = 42
+  override def part2(input: Iterator[String]): Int = {
+    val blueprints = input.map {
+      case s"Blueprint $id: Each ore robot costs $a ore. Each clay robot costs $b ore. Each obsidian robot costs $c ore and $d clay. Each geode robot costs $e ore and $f obsidian." =>
+        Blueprint(
+          id.toInt,
+          Vector(
+            Array(a.toInt, 0, 0, 0),       // ore robot
+            Array(b.toInt, 0, 0, 0),       // clay robot
+            Array(c.toInt, d.toInt, 0, 0), // obsidian robot
+            Array(e.toInt, 0, f.toInt, 0)  // geode robot
+          )
+        )
+    }
+    blueprints.take(3).map(bp => geodes(bp, 32)).product
+  }
 }

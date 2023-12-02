e00524d6e283ed870c8c5580f913ed838da1b4f4
Yann Moisan <yann@zen.ly>
2023-12-02 21:07:03 +0100

    improve input.sh
    Argument check
    Comments

--- input.sh
+++ input.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-curl https://adventofcode.com/$1/day/$2/input -o ~/input$2 --cookie "session=$3"
+
+# Check if the required arguments are provided
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <session_cookie> <year> <day>"
+    exit 1
+fi
+
+# Define the URL and output file
+url="https://adventofcode.com/$2/day/$3/input"
+output_file="$2/src/main/resources/input$3"
+
+# Use curl to download the input file
+curl "$url" -o "$output_file" --cookie "session=$1"
